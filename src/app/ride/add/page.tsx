9ce65b49df8926fa60b7091290ed274b311cdf89
devashishHp04 <devashish04.hp@gmail.com>
2024-09-10 15:09:31 +0530

    sadfas

--- src/app/ride/add/page.tsx
+++ src/app/ride/add/page.tsx
@@ -1,6 +1,23 @@
 import { AddRidePage } from '@/components/page'
+import { Metadata, ResolvingMetadata } from 'next';
 import React from 'react'
 
+export async function generateMetadata(
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+
+  return {
+    title: "Ride add",
+    description:  "Details of the add.",
+    openGraph: {
+      title: "Ride Details",
+      description: "Details of the ride.",
+      images: [
+         "https://dev.chasingwatts.com/ridepictures/ridepicture_32497_981.png" 
+      ],
+    },
+  };
+}
 const AddRide = () => {
   return (
     <AddRidePage />
